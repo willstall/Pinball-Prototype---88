706a53eae0a2857030f4c84bca18cf64cd22c1e7
William Stallwood <will@cipherprime.com>
2015-12-27 19:18:57 -0500

    Started Intractable

---
+++ Assets/project/HorizontalMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalMovement : MonoBehaviour
+{
+	public float movementSpeed = 1.0f;
+	public ForceMode mode = ForceMode.Impulse;
+	Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			rb.AddForce( Vector3.left * movementSpeed, mode );
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			rb.AddForce( Vector3.right * movementSpeed, mode );
+		}		
+	}
+}
---
+++ Assets/project/Interactable.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour
+{
+	[Range(0.0f,1.0f)]
+	public float idleAlpha = 0.2f;
+	[Range(0.0f,1.0f)]
+	public float activeAlpha = 0.7f;
+	[Range(0.0f,1.0f)]
+	public float selectAlpha = 0.45f;
+
+	Material mat;
+	Color color;
+	
+	void Start ()
+	{
+		mat = GetComponent<Renderer>().material;
+		color = mat.color;
+		Idle();
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	void Idle()
+	{
+		color.a = idleAlpha;
+		mat.color = color;
+	}
+
+	void Active()
+	{
+		color.a = activeAlpha;
+		mat.color = color;
+	}
+
+	void Select()
+	{
+		color.a = selectAlpha;
+		mat.color = color;
+	}
+}
